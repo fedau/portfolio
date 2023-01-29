132c352d4a4eba5edc3d27e8275a31465f8ab460
fedau <fedau.d@hotmail.com>
2023-01-29 20:29:38 +0000

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,32 +6,32 @@ import {FaLinkedin} from 'react-icons/fa'
 import { Link } from "react-router-dom";
 
 const Home = () => (
-  <div className="outer-wrapper">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
-    <h1 className="name">Fedau Depraetere</h1>
-    <h3>I am a software developper I have made different projects these past few months please check them out </h3>
-    <a href="https://github.com/fedau"><GoMarkGithub color='#283D3B' fontSize="2em" margin='2em'/> </a> 
-    <a href="https://www.linkedin.com/in/fedau-depraetere-601321209?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bzs%2Bh%2BtYGR3udwPKX7pRB3g%3D%3D"> <FaLinkedin color='#283D3B' fontSize="2em"/></a>
-    <div className="card-wrapper">
-<Link className="about" to="/about">
-    <div > <h4>About</h4> <p>Get to know me</p></div>
-</Link>
-<Link className="pythonP" to='/recipe'>
-    <div > <h4>Recipe Website</h4> <p>Made with Python - Flask - PostgresSql </p></div>
-</Link>
-<Link className="shelterP" to='/shelter'>
-    <div > <h4>Animal Shelter Website</h4> <p>Made with ReactJS - Mongodb </p></div>
-</Link>
-
-<Link className="shelterP" to='/shelter'>
-    <div > <h4>Another Project</h4> <p>mad with java or something </p></div>
-</Link>
-<Link className="shelterP" to='/shelter'>
-    <div > <h4>Another Project</h4> <p>mad with java or something </p></div>
-</Link>
+    <div className="outer-wrapper">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+      <h1 className="name">Fedau Depraetere</h1>
+      <h3>I am a software developper I have made different projects these past few months please check them out </h3>
+      <a href="https://github.com/fedau"><GoMarkGithub color='#283D3B' fontSize="2em" margin='2em'/> </a> 
+      <a href="https://www.linkedin.com/in/fedau-depraetere-601321209?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bzs%2Bh%2BtYGR3udwPKX7pRB3g%3D%3D"> <FaLinkedin color='#283D3B' fontSize="2em"/></a>
+      <div className="card-wrapper">
+  <Link className="about" to="/about">
+      <div > <h4>About</h4> <p>Get to know me</p></div>
+  </Link>
+  <Link className="pythonP" to='/recipe'>
+      <div > <h4>Recipe Website</h4> <p>Made with Python - Flask - PostgresSql </p></div>
+  </Link>
+  <Link className="shelterP" to='/shelter'>
+      <div > <h4>Animal Shelter Website</h4> <p>Made with ReactJS - Mongodb </p></div>
+  </Link>
+  
+  <Link className="pythonP" to='/shelter'>
+      <div > <h4>Another Project</h4> <p>mad with java or something </p></div>
+  </Link>
+  <Link className="shelterP" to='/shelter'>
+      <div > <h4>Another Project</h4> <p>mad with java or something </p></div>
+  </Link>
+      </div>
+  
     </div>
-
-  </div>
-);
-
-export default Home;
+  );
+  
+  export default Home;
--- src/components/ShelterProject.js
+++ src/components/ShelterProject.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import video from '../public/shelter.mp4'
+import '../App.css';
+
 
 const ShelterProject = () => {
   return (
     <div>
-      this is the shelter
+      <h2> This is the Animal shelter project </h2>
+      <p> This project was made in ReactJS with mongodb. It was a group project and we had 6 days to come up with a brief, plan and code the whole project. <br/>
+        It was a great experience working in team, delegating tasks and helping each other out. 
+        You can checkout the video of a walk through or look at the code in github linked <a href='https://github.com/fedau/pet-shelter-app'>here </a> .
+      </p>
+      <video width="320" height="240" controls>
+       <source src={video} type="video/mp4"/>
+        Your browser does not support the video tag.
+        </video> 
+        <a href="/"><button>Home</button></a>
     </div>
   )
 }
